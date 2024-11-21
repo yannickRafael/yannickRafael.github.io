7f88936e6af8171d4723c01a80d04c2094d83a33
Yannick Rafael Matimbe <125164172+yannickRafael@users.noreply.github.com>
2024-11-21 21:08:00 +0200

    added function to fetch data

---
+++ index.js
@@ -0,0 +1,14 @@
+async function getData() {
+    const url = "https://imdb-api.projects.thetuhin.com/search?query=Avengers";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
